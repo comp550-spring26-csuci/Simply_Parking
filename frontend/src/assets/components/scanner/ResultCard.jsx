501b3758e5fab2ef7aaf330bcb1678fbcdf6f5ea
paramveersingh <param9738@gmail.com>
2026-03-10 19:53:59 -0700

    setup frontend

---
+++ frontend/src/assets/components/scanner/ResultCard.jsx
@@ -0,0 +1,21 @@
+export default function ResultCard(){
+
+  return(
+    <div className="card">
+
+      <h3>Scan Result</h3>
+
+      <p>
+        Plate Number:
+        <strong> MH12AB1234 </strong>
+      </p>
+
+      <p>
+        Confidence:
+        <strong> 96% </strong>
+      </p>
+
+    </div>
+  )
+
+}
---
+++ frontend/src/data/mockData.js
@@ -0,0 +1,21 @@
+export default [
+
+{
+id:1,
+plate:"MH12AB1234",
+time:"10:21 AM"
+},
+
+{
+id:2,
+plate:"DL05CD9999",
+time:"11:03 AM"
+},
+
+{
+id:3,
+plate:"KA03XY3333",
+time:"12:40 PM"
+}
+
+]
