d8b9c9c97d6b0242d469f83d9d32c28b14baaa5e
A Torres <user@Torres-MacBook-Air.local>
2026-03-20 17:09:54 -0700

    Add dbreader.py

---
+++ dbreader.py
@@ -0,0 +1,8 @@
+from database_manager import DatabaseManager
+
+def main():
+with DatabaseManager() as db:
+rows = db.fetch_all(limit=50) # change limit or set None for all
+
+for (id_, plate, source_file, ts) in rows:
+    print(f"{id_:>5}  {plate:<10}  {ts}  {source_file}")
